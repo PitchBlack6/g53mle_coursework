f7ebadf1c04c5cf3687768240a12de7bd8f4ac8c
tenryuuwu <30344219+tenryuuwu@users.noreply.github.com>
2019-11-06 15:01:36 +0000

    Added normalize_coordinate function
    Also called normalize_coordinate function in obtain_data function

--- Regression/data.py
+++ Regression/data.py
@@ -2,11 +2,20 @@ import numpy as np
 import tensorflow as tf
 import math
 import logging
+from sklearn.preprocessing import MinMaxScaler
 logging.basicConfig(level=logging.DEBUG)
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 import pandas as pd
 from sklearn.model_selection import KFold
+
+def normalize_coordinate(coordinate):
+    #oordinate = np.reshape(coordinate, (-1, 1))
+    scalar = MinMaxScaler(feature_range=(0, 1))
+    scalar = scalar.fit(coordinate)
+    normalized = scalar.transform(coordinate)
+    #normalized = np.reshape(normalized, -1)
+    return normalized
 
 def obtain_data():
     # Data preprocessing
@@ -17,11 +26,16 @@ def obtain_data():
     y_array = np.asarray(y, dtype = "float")
     y_arrayt=y_array
     whole_data=np.concatenate((x_arrayt, y_arrayt),axis=1)
+    print("whole data: ", whole_data)
     angle = pd.read_csv("angle.csv", header=0)
     angle_array = np.asarray(angle, dtype = "float")
-    angle_arrayt=angle_array.transpose()
+    #angle_arrayt=angle_array.transpose()
     label=angle_array#+1e-50-1e-50
-    temp=np.array([angle_array[:,0]])
-    features = (whole_data - 200) / 2000  # 367616, 98
-    labels = temp.transpose()  # 367616, 1
+    #temp=np.array([angle_array[:,0]])
+    #features = (whole_data - 200) / 2000  # 367616, 98
+    features = normalize_coordinate(whole_data)
+    #labels = temp.transpose()  # 367616, 1
     return features, label
+
+
+obtain_data()
