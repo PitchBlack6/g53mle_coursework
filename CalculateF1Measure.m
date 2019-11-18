642d159205377c555b96f7ef04cf7ac31a682655
ctchern20 <36656561+ctchern20@users.noreply.github.com>
2019-11-18 23:39:06 +0000

    Delete CalculateF1Measure.m

--- CalculateF1Measure.m
+++
@@ -1,8 +0,0 @@
-function [measure] = CalculateF1Measure(precision,recall)
-%CALCULATEF1MEASURE Summary of this function goes here
-%   Detailed explanation goes here
-
-measure = (2*recall*precision)/(recall+precision);
-
-end
-
