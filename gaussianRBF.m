24b7670fdd3c20492f7aea52473ac3e5e69c1d59
fawzazrn <43840156+fawzazrn@users.noreply.github.com>
2019-12-06 20:30:21 +0000

    Gaussian RBF script

---
+++ gaussianRBF.m
@@ -0,0 +1,82 @@
+train_size = 10000;
+test_size = 1000;
+
+label = csvread('angle.csv',1,0);
+featureX = load('predx_for_classification.csv');
+featureY = load('predy_for_classification.csv');
+disp('Data Loaded');
+
+featureX = normaliseData(featureX);
+featureY = normaliseData(featureY);
+disp('Data Normalized');
+
+% get first column
+labels = label(:,1);
+
+% transpose 
+% labels = temp.';
+
+features = [featureX featureY];
+
+% Shuffle data
+% [features, labels] = shuffleData(features, labels);
+% disp('Data Shuffled');
+
+% Train test split
+trainFeatures = features(1:train_size, :);
+trainLabels = labels(1:train_size);
+disp(max(trainLabels));
+testFeatures = features(train_size+1:train_size+test_size, :);
+testLabels = labels(train_size+1:train_size+test_size);
+disp('Data Split');
+
+% k-fold
+[trainFeatures, trainLabels] = spiltDataset(trainFeatures, trainLabels);
+
+% Model 1
+sumWeights = 0;
+sumBias = 0;
+sumRMSE = 0;
+
+% Cross-validate two SVM regression models using 5-fold cross-validation. 
+% For both models, specify to standardize the predictors. 
+% For one of the models, specify to train using the default linear kernel, and the Gaussian kernel for the other model.
+% Mdl = fitrsvm(trainFeatures,trainLabels, 'Standardized', true, 'Kfold', 5);
+
+% train data
+disp('train data');
+for n = 1:10 
+    dummyFeature = trainFeatures;
+    dummyFeature(n, :, :) = [];
+    dummyLabel = trainLabels;
+    dummyLabel(n, :, :) = [];
+    
+    currentTrainFeatures = reshape(dummyFeature, size(trainFeatures, 2) * 9, size(trainFeatures, 3));
+    currentTrainLabels = reshape(dummyLabel, size(trainLabels, 2)*9, size(trainLabels, 3));
+    
+    % using standardized data gives lower average RMSE
+    Mdl1 = fitrsvm(currentTrainFeatures, currentTrainLabels, 'Standardize', true, 'KernelFunction', 'gaussian', 'BoxConstraint', 1);
+    sigma = Mdl1.Sigma;
+    
+    % sumWeights = sumWeights + Mdl1.Beta;
+    sumBias = sumBias + Mdl1.Bias;
+    
+    % make predictions using support vectors
+    predictions = predict(Mdl1, testFeatures);
+    
+    % get RMSE by comparing predictions with testLabels
+    sumRMSE = sumRMSE + sqrt(mean((predictions - testLabels).^2));
+   
+end
+
+
+% disp average RMSE
+disp(sumRMSE/10);
+
+% sigma is a vector of 1x98
+disp(sumSigma/10);
+
+
+
+
+
